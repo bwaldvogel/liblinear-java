a5d6edc276e1b33e8879137c3cca0d4f4e205f36
Benedikt Waldvogel <mail at bwaldvogel.de>
2009-06-17 23:24:30 +0200

    Parameter: added getters and cleaned up Javadoc
    Thanks to Zeno Gantner.

--- src/java/liblinear/Parameter.java
+++ src/java/liblinear/Parameter.java
@@ -3,25 +3,6 @@ package liblinear;
 import static liblinear.Linear.copyOf;
 
 
-/**
- *  C is the cost of constraints violation. (we usually use 1 to 1000)
- *  eps is the stopping criterion. (we usually use 0.01).
- *
- *  nr_weight, weight_label, and weight are used to change the penalty
- *  for some classes (If the weight for a class is not changed, it is
- *  set to 1). This is useful for training classifier using unbalanced
- *  input data or with asymmetric misclassification cost.
- *
- *  nr_weight is the number of elements in the array weight_label and
- *  weight. Each weight[i] corresponds to weight_label[i], meaning that
- *  the penalty of class weight_label[i] is scaled by a factor of weight[i].
- *
- *  If you do not want to change penalty for any of the classes,
- *  just set nr_weight to 0.
- *
- *  *NOTE* To avoid wrong parameters, check_parameter() should be
- *  called before train().
- */
 public final class Parameter {
 
    double     C;
@@ -41,6 +22,18 @@ public final class Parameter {
       setEps(eps);
    }
 
+   /**
+    * <p>nr_weight, weight_label, and weight are used to change the penalty
+    * for some classes (If the weight for a class is not changed, it is
+    * set to 1). This is useful for training classifier using unbalanced
+    * input data or with asymmetric misclassification cost.</p>
+    *
+    * <p>Each weight[i] corresponds to weight_label[i], meaning that
+    * the penalty of class weight_label[i] is scaled by a factor of weight[i].</p>
+    *
+    * <p>If you do not want to change penalty for any of the classes,
+    * just set nr_weight to 0.</p>
+    */
    public void setWeights( double[] weights, int[] weightLabels ) {
       if ( weights == null ) throw new IllegalArgumentException("'weight' must not be null");
       if ( weightLabels == null || weightLabels.length != weights.length )
@@ -49,23 +42,59 @@ public final class Parameter {
       this.weight = copyOf(weights, weights.length);
    }
 
+   /**
+    * @see #setWeights(double[], int[])
+    */
+   public double[] getWeights() {
+      return copyOf(weight, weight.length);
+   }
+
+   /**
+    * @see #setWeights(double[], int[])
+    */
+   public int[] getWeightLabels() {
+      return copyOf(weightLabel, weightLabel.length);
+   }
+
+   /**
+    * the number of weights
+    * @see #setWeights(double[], int[])
+    */
    public int getNumWeights() {
       if ( weight == null ) return 0;
       return weight.length;
    }
 
+   /**
+    * C is the cost of constraints violation. (we usually use 1 to 1000)
+    */
    public void setC( double C ) {
       if ( C <= 0 ) throw new IllegalArgumentException("C must not be <= 0");
       this.C = C;
    }
 
+   public double getC() {
+      return C;
+   }
+
+   /**
+    * eps is the stopping criterion. (we usually use 0.01).
+    */
    public void setEps( double eps ) {
       if ( eps <= 0 ) throw new IllegalArgumentException("eps must not be <= 0");
       this.eps = eps;
    }
 
+   public double getEps() {
+      return eps;
+   }
+
    public void setSolverType( SolverType solverType ) {
       if ( solverType == null ) throw new IllegalArgumentException("solver type must not be null");
       this.solverType = solverType;
+   }
+
+   public SolverType getSolverType() {
+      return solverType;
    }
 }
