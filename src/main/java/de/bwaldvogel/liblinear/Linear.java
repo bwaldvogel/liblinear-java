c9f4eb25ddddf41be292474354e92cb0767952fb
Benedikt Waldvogel <mail at bwaldvogel.de>
2013-11-14 23:24:16 +0100

    version 1.94

--- src/main/java/de/bwaldvogel/liblinear/Linear.java
+++ src/main/java/de/bwaldvogel/liblinear/Linear.java
@@ -28,7 +28,7 @@ import java.util.regex.Pattern;
  *
  * <p><em>The port was done by Benedikt Waldvogel (mail at bwaldvogel.de)</em></p>
  *
- * @version 1.92
+ * @version 1.94
  */
 public class Linear {
 
