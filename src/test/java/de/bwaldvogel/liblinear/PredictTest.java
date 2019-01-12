cb6db98e8639d8bbabf10475ee1383e1e0ecf15a
Benedikt Waldvogel <mail@bwaldvogel.de>
2019-01-12 18:20:17 +0100

    Improve PredictTest

--- src/test/java/de/bwaldvogel/liblinear/PredictTest.java
+++ src/test/java/de/bwaldvogel/liblinear/PredictTest.java
@@ -1,16 +1,22 @@
 package de.bwaldvogel.liblinear;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.PrintStream;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.nio.file.Files;
+import java.util.List;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 
 public class PredictTest {
@@ -19,17 +25,29 @@ public class PredictTest {
     private StringBuilder sb        = new StringBuilder();
     private Writer        writer    = new StringWriter();
 
+    private ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
     @Before
     public void setUp() {
-        System.setOut(mock(PrintStream.class)); // dev/null
+        Linear.resetRandom();
+        Linear.setDebugOutput(new PrintStream(byteArrayOutputStream));
         assertThat(testModel.getNrClass()).isGreaterThanOrEqualTo(2);
         assertThat(testModel.getNrFeature()).isGreaterThanOrEqualTo(10);
     }
 
-    private void testWithLines(StringBuilder sb) throws Exception {
-        BufferedReader reader = new BufferedReader(new StringReader(sb.toString()));
+    @After
+    public void tearDown() {
+        Linear.enableDebugOutput();
+    }
 
-        Predict.doPredict(reader, writer, testModel);
+    private void testWithLines(StringBuilder sb) throws Exception {
+        try (StringReader stringReader = new StringReader(sb.toString());
+             BufferedReader reader = new BufferedReader(stringReader)) {
+            Predict.doPredict(reader, writer, testModel);
+        }
     }
 
     @Test(expected = RuntimeException.class)
@@ -50,5 +68,20 @@ public class PredictTest {
         sb.append(testModel.label[1]).append(" 2:-71.555   9:88223").append("\n");
         testWithLines(sb);
         assertThat(writer.toString()).isNotEmpty();
+    }
+
+    @Test
+    public void testTrainAndPredict() throws Exception {
+        String modelFile = temporaryFolder.newFile("model").toString();
+        Train.main(new String[] {"-s", "0", "src/test/datasets/dna.scale/dna.scale", modelFile});
+
+        File predictionsFile = temporaryFolder.newFile("predictions");
+        Predict.main(new String[] {"-b", "1", "src/test/datasets/dna.scale/dna.scale.t", modelFile, predictionsFile.toString()});
+        List<String> predictions = Files.readAllLines(predictionsFile.toPath(), Linear.FILE_CHARSET);
+        assertThat(predictions).hasSize(1187);
+        assertThat(predictions.get(0)).isEqualTo("labels 3 1 2");
+
+        String loggedString = byteArrayOutputStream.toString();
+        assertThat(loggedString).contains("Accuracy = 94.9410% (1126/1186)");
     }
 }
